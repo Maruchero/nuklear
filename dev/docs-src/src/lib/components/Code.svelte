a5bd71b462f0e58778cabf750685a0620a93e1cc
Maruchero <marce.gnz303@gmail.com>
2023-01-03 23:41:22 +0100

    Add Code component

---
+++ dev/docs-src/src/lib/components/Code.svelte
@@ -0,0 +1,35 @@
+<script>
+  // import hljs from "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/es/highlight.min.js";
+  import { onMount } from "svelte";
+
+  export const lang = "html";
+
+  let className = "language-" + lang;
+
+  function highlight() {
+    if (hljs) hljs.highlightAll();
+  }
+
+  onMount(highlight);
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/atom-one-dark.min.css"
+  />
+  <script
+    src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"
+    on:load={highlight}
+  ></script>
+</svelte:head>
+
+<pre>
+  <code class={className}><slot /></code>
+</pre>
+
+<style>
+  code {
+    border-radius: 5px;
+  }
+</style>
